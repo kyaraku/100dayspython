4621ea4a8dd5983f94e04c92d485c10ffca312ed
jodi <kyara402@gmail.com>
2021-12-11 15:26:46 -0500

    Update tipcalculator.py
    Updated tipAmount and added the f-string format for the display

--- tipcalculator.py
+++ tipcalculator.py
@@ -8,11 +8,11 @@ def calculateTip():
     numberOfPersons = int(input("How many people to split the bill? "))
     tip = int(input("What percenatge tip would you like to give? 10, 12, or 15? "))
     tip_percentage = tip / 100
-    tipAmount = totalBill * tip_percentage
-    print("Tip Amount = ${}".format(round(tipAmount, 2)))
+    tipAmount = round((totalBill * tip_percentage), 2)
+    print(f"Tip Amount = ${tipAmount}")
     amountPerPerson = (totalBill + tipAmount) / numberOfPersons
-    final_amount = round(amountPerPerson)
-    print("Each person should pay: ${}".format(final_amount, 2))
+    final_amount = round(amountPerPerson, 2)
+    print(f"Each person should pay: ${final_amount}")
     return final_amount
 
 main()
