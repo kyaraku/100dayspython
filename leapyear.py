1647a2645dab543c633aee634b3a61c3fd716a6e
jodi <kyara402@gmail.com>
2021-12-12 20:27:51 -0500

    Update leapyear.py
    leapyear logic updated

--- leapyear.py
+++ leapyear.py
@@ -1,23 +1,25 @@
+def main():
+    print("Welcome to the Leap Year Checker!")
+    year = int(input("Enter a year you would like to check for Leap Year: "))
+    print(is_leap(year))
+
 def is_leap(year):
     leap = False
     
-    # Write your logic here
-    # if year % 4 == 0:
-    #     return True
-    #     if (year % 100 == 0):
-    #         return False
-    #         if year % 400 == 0:
-    #             return True
-    #         else:
-    #             return False
-    #     else:
-    #         return True
-    # else:
-    #     return False
+    # Logic below
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(f"{year} is a Leap Year")
+            else:
+                print(f"{year} is not a Leap Year")
+        else:
+            print(f"{year} is a Leap Year")
+    else:
+        print(f"{year} is not a Leap Year")
         
-    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+    #return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 
     return leap
 
-year = int(input())
-print(is_leap(year))
+main()
