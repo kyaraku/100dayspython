f1c851bde5df2a661bc206a9bca710a536fa6970
jodi <kyara402@gmail.com>
2021-12-13 10:37:18 -0500

    Update oddorevencheck.py
    updated to include functions

--- oddorevencheck.py
+++ oddorevencheck.py
@@ -1,6 +1,15 @@
-number = int(input("Which number do you want to check? "))
 
-if number % 2 == 0:
-    print(f"The number {number} is an even number")
-else:
-    print(f"The number {number} is an odd number")
+def main():
+    print("Welcome to the odd or even checker!")
+    print("Follow the prompts to check if a number is even or odd.")
+    odd_even()
+
+def odd_even():
+    number = int(input("Which number do you want to check? "))
+    
+    if number % 2 == 0:
+        print(f"The number {number} is an even number")
+    else:
+        print(f"The number {number} is an odd number")
+        
+main()
